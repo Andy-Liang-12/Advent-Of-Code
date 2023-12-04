b2fdab84bbc4aa4e0c87dadfc00458eb918ff5cd
Andy-Liang-12 <137653883+Andy-Liang-12@users.noreply.github.com>
2023-12-04 07:40:56 -0500

    Update Day4p2.py

--- Day 4/Day4p2.py
+++ Day 4/Day4p2.py
@@ -28,26 +28,3 @@ print(card_wins)
 print(num_of_cards)
 
 print(sum(num_of_cards))
-
-# current_scratchcard = 0
-# total_scratchcards = 1
-# scratchcards_left = 1
-
-# while(scratchcards_left>0 and current_scratchcard<len(lines)):
-#     current_scratchcard+=1
-#     scratchcards_left-=1
-#     line = lines[total_scratchcards].split(':')[1]
-#     number_arr = line.split('|')
-#     winning_numbers=number_arr[0].split()
-#     your_numbers=number_arr[1].split()
-#     score = 0
-#     for num in your_numbers:
-#         if num in winning_numbers:
-#             score+=1
-#     scratchcards_left+=score
-#     total_scratchcards+=score
-    
-# print(total_scratchcards)
-
-
-
